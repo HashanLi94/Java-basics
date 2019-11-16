83e67c162c9787c929c77eeae8f12ae6db2997e5
HashanLi94 <hashanduplessis@gmail.com>
2019-11-16 13:55:40 +0530

    Create Applied.java

---
+++ src/Example/Applied.java
@@ -0,0 +1,21 @@
+package Example;
+
+import jdk.nashorn.internal.parser.JSONParser;
+
+public class Applied {
+    int students;
+    String name;
+    String subject;
+
+    void activities(){
+        System.out.println("test");
+        System.out.println(students);
+        System.out.println(name);
+    }
+
+    void sports(){
+        System.out.println("do something");
+    }
+}
+
+
